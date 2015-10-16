0e1607a76f74430a35447baa92a1e80b1a3b27ba
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-16 20:51:50 +0330

    delete build.rs

--- build.rs
+++
@@ -1,4 +0,0 @@
-system("gcc -fPIC -c greetings.c -I ./include/")
-system("gcc -fPIC -c farewells.c -I ./include/")
-system("gcc -fPIC -c main.c -I ./include/")
-system("gcc *.o -o main -I ./include/")
